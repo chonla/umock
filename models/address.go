c1a47753759405fb582ccb1e0566fd1eff5c9f7e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2021-06-30 11:38:09 +0700

    Add PORT missing handler

--- models/address.go
+++ models/address.go
@@ -1,6 +1,10 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type Address struct {
 	Host string `yaml:"host"`
@@ -8,5 +12,13 @@ type Address struct {
 }
 
 func (a Address) String() string {
+	if a.Port == 0 {
+		envPort := os.Getenv("PORT")
+		parsedEnvPort, err := strconv.ParseInt(envPort, 10, 64)
+		if err != nil {
+			parsedEnvPort = int64(80)
+		}
+		a.Port = int(parsedEnvPort)
+	}
 	return fmt.Sprintf("%s:%d", a.Host, a.Port)
 }
